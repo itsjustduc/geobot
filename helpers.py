79a1afffc5329c42489f2edb07543a82f36cea8f
duc <092833@protonmail.com>
2023-09-23 11:13:26 +0200

    oop private posting 🥴

--- helpers.py
+++ helpers.py
@@ -4,7 +4,7 @@ def post_to_fedi(instance, status, token, cw):
     response = requests.post(
     url= instance + "api/v1/statuses",
     headers={"Authorization": "Bearer " + token},
-    json={"status": status, "visibility": "private", "spoiler_text": cw},
+    json={"status": status, "visibility": "public", "spoiler_text": cw},
     )
     if response.status_code != 200:
         print(response.text)
